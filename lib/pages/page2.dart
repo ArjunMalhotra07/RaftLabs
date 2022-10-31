afdceb0c7eb5bea8c11de4fbb2e7528b09ebf134
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-10-31 23:43:08 +0530

    Added assets

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:raftlabs/utils/api.dart';
+// import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class WelcomeMessage extends StatefulWidget {
   WelcomeMessage({Key? key}) : super(key: key);
@@ -12,13 +14,48 @@ class _WelcomeMessageState extends State<WelcomeMessage> {
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
     int hour = now.hour;
-    print(now.hour);
+    bool ans = false;
+    void checkingNetwork() async {
+      // ans = await InternetConnectionChecker().hasConnection;
+    }
+
+    @override
+    void initState() {
+      super.initState();
+      checkingNetwork();
+    }
+
+    print(ans.toString());
     return Center(
-      child: hour >= 0 && hour <= 11
-          ? const Text("Good Morning")
-          : hour >= 12 && hour <= 16
-              ? const Text("Good Afternoon")
-              : const Text("Good Evening"),
-    );
+        child: hour >= 0 && hour <= 11
+            ? const Text("Good Morning")
+            : hour >= 12 && hour <= 16
+                ? Column(
+                    children: [
+                      Image.asset(
+                        'assets/gif1.gif',
+                        height: 125.0,
+                        width: 125.0,
+                      ),
+                      const Text("Good Afternoon"),
+                    ],
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/gif2.gif',
+                        height: 300.0,
+                        width: 300.0,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        "Good Evening",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ],
+                  ));
   }
 }
--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,6 +1,7 @@
 import '../models/apiModel.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:io';
 
 class API {
   Future<List<SampleApi>?> getApiDataFunc() async {
@@ -9,6 +10,19 @@ class API {
     if (response.statusCode == 200) {
       var data = json.decode(response.body)['entries'] as List;
       return data.map((e) => SampleApi.fromJson(e)).toList();
+    }
+  }
+
+  Future<String?> CheckNetworkConnectivity() async {
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        print('connected');
+        return "connected";
+      }
+    } on SocketException catch (_) {
+      print('not connected');
+      return "not connected";
     }
   }
 }
