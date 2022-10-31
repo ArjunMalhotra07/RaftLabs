5eb95d6c4e99635970fcfd588c071ae4b8a2f551
Arjun Malhotra <arjun03.malhotra@gmail.com>
2022-10-31 21:31:17 +0530

    Added custom widgets

--- lib/pages/apiInfo.dart
+++ lib/pages/apiInfo.dart
@@ -2,12 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:raftlabs/models/apiModel.dart';
+import 'package:raftlabs/utils/customWidgets.dart';
 
 class FetchDataPage extends StatelessWidget {
   const FetchDataPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    const box = SizedBox(
+      height: 10,
+    );
     Future<List<SampleApi>?> getApiDataFunc() async {
       var response =
           await http.get(Uri.parse('https://api.publicapis.org/entries'));
@@ -33,29 +37,67 @@ class FetchDataPage extends StatelessWidget {
                         Text("Oops. Something went wrong. Try again later!")));
           } else if (data.hasData) {
             var incomingDataObjects = data.data as List<SampleApi>;
-            return ListView.builder(
-              itemCount: incomingDataObjects.length,
-              itemBuilder: ((context, index) {
-                final object = incomingDataObjects[index];
-                final apiName = object.api;
-                final desc = object.description;
-                final cors = object.cors;
-                final https = object.https;
-                return Container(
-                  child: Column(
-                    children: [
-                      Text(apiName.toString()),
-                      Text(desc.toString()),
-                      Text(cors.toString()),
-                      Text(https.toString()),
-                      SizedBox(
-                        height: 50,
-                      )
-                    ],
-                  ),
-                );
-              }),
-              scrollDirection: Axis.vertical,
+            return Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: ListView.builder(
+                itemCount: incomingDataObjects.length,
+                itemBuilder: ((context, index) {
+                  final object = incomingDataObjects[index];
+                  final apiName = object.api;
+                  final desc = object.description;
+                  final cors = object.cors;
+                  final https = object.https;
+                  var num = index + 1;
+                  return SizedBox(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CustomText(
+                          nameText: "$num. API Name",
+                        ),
+                        box,
+                        CustomFormFieldWidget(
+                          hintText: apiName.toString(),
+                        ),
+                        box,
+                        CustomText(
+                          nameText: "Description",
+                        ),
+                        box,
+                        CustomFormFieldWidget(
+                          hintText: desc.toString(),
+                        ),
+                        box,
+                        CustomText(
+                          nameText: "Cors",
+                        ),
+                        box,
+                        CustomFormFieldWidget(
+                          hintText: cors.toString(),
+                        ),
+                        box,
+                        CustomText(
+                          nameText: "Https",
+                        ),
+                        box,
+                        CustomFormFieldWidget(
+                          hintText: https.toString(),
+                        ),
+                        box,
+                        const Center(
+                            child: Text(
+                          "*********************",
+                          style: TextStyle(color: Colors.blue),
+                        )),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+                scrollDirection: Axis.vertical,
+              ),
             );
           } else {
             return const Center(child: CircularProgressIndicator());
---
+++ lib/utils/customWidgets.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomFormFieldWidget extends StatelessWidget {
+  var hintText;
+  CustomFormFieldWidget({Key? key, this.hintText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xffBCE0FD).withOpacity(.38),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      height: 50,
+      child: TextFormField(
+        readOnly: true,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.only(left: 20, top: 8),
+          hintStyle: const TextStyle(
+            color: Colors.black,
+            fontSize: 14,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w400,
+          ),
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomText extends StatelessWidget {
+  var nameText;
+  CustomText({Key? key, this.nameText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      nameText,
+      style: const TextStyle(
+        fontSize: 14,
+        color: Colors.blue,
+        fontFamily: 'Poppins',
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+}
